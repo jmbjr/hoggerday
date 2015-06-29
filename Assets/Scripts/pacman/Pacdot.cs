0f3a55e60bf74ee82d8728a04b9f18f2bcf0d211
John Boyle <boylejm@gmail.com>
2015-06-28 21:54:46 -0700

    new script for pacdots

---
+++ Assets/Scripts/pacman/Pacdot.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pacdot : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D co) {
+		//DO Stuff
+		if (co.name == "pacman")
+			Destroy(gameObject);
+	}
+}
