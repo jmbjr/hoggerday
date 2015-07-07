0da9c5ff8f7674081aacb8552cd8fa019247c502
John Boyle <boylejm@gmail.com>
2015-07-07 01:21:11 -0700

    fix script to actually instantiate pacdots

--- Assets/Scripts/pacman/LoadPacdots.cs
+++ Assets/Scripts/pacman/LoadPacdots.cs
@@ -1,25 +1,26 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 using System.IO;
 
 public class LoadPacdots : MonoBehaviour {
-	public TextAsset dataFile;
-
 	// Use this for initialization
 	void Start () {
 		string input = File.ReadAllText( @"c:\dev\hoggerday\Assets\Maps\pacman\level1.txt" );
 
 		int i = 0, j = 0;
-		int[,] result = new int[32, 29];
+		int numrows = 33, numcols = 30;
+		int[,] result = new int[numrows, numcols];
 		foreach (var row in input.Split('\n')) {
 			j = 0;
 			foreach (var col in row.Trim().Split(',')) {
-				Debug.Log ("another row");
-
-				// result[i, j] = int.Parse(col.Trim());
 				try {
 					int.TryParse(col.Trim(), out result[i, j]);
-					Debug.Log (i.ToString() + "," + j.ToString() + " : " + result[i, j]);
+					if (result[i,j] == 0) {
+						Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/pacman/pacdot.prefab", typeof(GameObject));
+						GameObject clone = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
+						clone.transform.position = new Vector3(j, numrows - i -1, 0);
+					}
 				}
 				catch { break;}
 				j++;
