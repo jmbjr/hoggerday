c2dfa2be22d43cc1b0f66634fea87c36c8f42ffa
John Boyle <boylejm@gmail.com>
2015-07-07 00:01:22 -0700

    try another approach at reading map txt file

--- Assets/Scripts/pacman/LoadPacdots.cs
+++ Assets/Scripts/pacman/LoadPacdots.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class LoadPacdots : MonoBehaviour {
 	public TextAsset dataFile;
 
 	// Use this for initialization
 	void Start () {
-		string[] dataLines = dataFile.text.Split('\n'); 
+		string input = File.ReadAllText( @"c:\dev\hoggerday\Assets\Maps\pacman\level1.txt" );
 
-		int lineNum = 0;
-		foreach (string line in dataLines)
-		{
-			Debug.Log(line);
+		int i = 0, j = 0;
+		int[,] result = new int[32, 29];
+		foreach (var row in input.Split('\n')) {
+			j = 0;
+			foreach (var col in row.Trim().Split(',')) {
+				Debug.Log ("another row");
+
+				// result[i, j] = int.Parse(col.Trim());
+				try {
+					int.TryParse(col.Trim(), out result[i, j]);
+					Debug.Log (i.ToString() + "," + j.ToString() + " : " + result[i, j]);
+				}
+				catch { break;}
+				j++;
+			}
+			i++;
 		}
 	}
 
