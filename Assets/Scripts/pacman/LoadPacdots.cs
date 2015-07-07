c2446e7a78c5279ae2a1b53c46fbe43a90cf6923
John Boyle <boylejm@gmail.com>
2015-07-06 23:07:20 -0700

    add basic framework for loading pacdots

---
+++ Assets/Scripts/pacman/LoadPacdots.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadPacdots : MonoBehaviour {
+	public TextAsset dataFile;
+
+	// Use this for initialization
+	void Start () {
+		string[] dataLines = dataFile.text.Split('\n'); 
+
+		int lineNum = 0;
+		foreach (string line in dataLines)
+		{
+			Debug.Log(line);
+		}
+	}
+
+}
