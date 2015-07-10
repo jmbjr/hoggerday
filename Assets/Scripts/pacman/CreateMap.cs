596fc7952046bfb72c42213e459ef845a4ecb0a8
John Boyle <boylejm@gmail.com>
2015-07-10 01:28:37 -0700

    added all 2x walls and corners (no tees)

--- Assets/Scripts/pacman/CreateMap.cs
+++ Assets/Scripts/pacman/CreateMap.cs
@@ -56,19 +56,73 @@ public class CreateMap : MonoBehaviour {
 
 					switch (theCode)
 					{
+						// CORNER 2X WALL
 						case "xxxx22x20": //top left corner 2x wall
-							thePrefab="Assets/Prefabs/pacman/wall_2x_corner_tl.prefab";
+							thePrefab="Assets/Prefabs/pacman/wall_2x_corner.prefab";
 							Zrot = 0;
 							Xoff = 0.5f;
 							Yoff = -0.5f;
 							break;
+						case "xxx22x02x": //top right corner 2x wall
+							thePrefab="Assets/Prefabs/pacman/wall_2x_corner.prefab";
+							Zrot = -90;
+							Xoff = 0.5f;
+							Yoff = 0.5f;
+							break;							
+						case "02x22xxxx": //bot right corner 2x wall
+							thePrefab="Assets/Prefabs/pacman/wall_2x_corner.prefab";
+							Zrot = 180;
+							Xoff = 1.5f;
+							Yoff = 0.5f;
+							break;
+						case "x20x22xxx": //bot left corner 2x wall
+							thePrefab="Assets/Prefabs/pacman/wall_2x_corner.prefab";
+							Zrot = 90;
+							Xoff = 1.5f;
+							Yoff = -0.5f;
+							break;			
+						// FLAT 2x WALL
 						case "x20x20x20": //flat 2x wall. left side
-							thePrefab="Assets/Prefabs/pacman/wall_2x_flat_l.prefab";
+						case "x21x20x20":
+						case "x22x20x20":
+						case "x20x20x21":
+						case "x20x20x22":
+							thePrefab="Assets/Prefabs/pacman/wall_2x_flat.prefab";
 							Zrot = -90;
 							Xoff = 0;
-							Yoff = 0;
+							Yoff = 0.5f;
 							break;
-						default:
+						case "xxx222000": //flat 2x wall. top side
+						case "xxx222100":
+						case "xxx222001":
+						case "xxx222002": 
+						case "xxx222200": 
+							thePrefab="Assets/Prefabs/pacman/wall_2x_flat.prefab";
+							Zrot = 0;
+							Xoff = 0.5f;
+							Yoff = -0.5f;
+							break;							
+						case "02x02x02x": //flat 2x wall. right side
+						case "22x02x02x":
+						case "02x02x22x":
+						case "12x02x02x":
+						case "02x02x12x":
+							thePrefab="Assets/Prefabs/pacman/wall_2x_flat.prefab";
+							Zrot = 90;
+							Xoff = 2;
+							Yoff = -0.5f;
+							break;	
+						case "000222xxx": //flat 2x wall. bottom side
+						case "001222xxx":
+						case "100222xxx":
+						case "200222xxx":
+						case "002222xxx":
+							thePrefab="Assets/Prefabs/pacman/wall_2x_flat.prefab";
+							Zrot = 180;
+							Xoff = 1.5f;
+							Yoff = 0.5f;
+							break;						
+						default:					
 							thePrefab="";
 							Zrot = 0;
 							Xoff = 0;
