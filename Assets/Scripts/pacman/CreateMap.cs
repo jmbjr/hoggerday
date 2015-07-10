acfe53e986b884e1aafdddb7885778d59e1b371a
John Boyle <boylejm@gmail.com>
2015-07-09 21:49:56 -0700

    don't double up outer walls

--- Assets/Scripts/pacman/CreateMap.cs
+++ Assets/Scripts/pacman/CreateMap.cs
@@ -9,7 +9,7 @@ public class CreateMap : MonoBehaviour {
 		string input = File.ReadAllText( @"c:\dev\hoggerday\Assets\Maps\pacman\level1.txt" );
 
 		int i = 0, j = 0;
-		int numrows = 33, numcols = 30;
+		int numrows = 31, numcols = 28;
 		int[,] result = new int[numrows, numcols];
 		foreach (var row in input.Split('\n')) {
 			j = 0;
@@ -19,7 +19,7 @@ public class CreateMap : MonoBehaviour {
 					if (result[i,j] == 0) {
 						Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/pacman/pacdot.prefab", typeof(GameObject));
 						GameObject clone = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
-						clone.transform.position = new Vector3(j, numrows - i -1, 0);
+						clone.transform.position = new Vector3(j+1, numrows - i , 0);
 					}
 				}
 				catch { break;}
