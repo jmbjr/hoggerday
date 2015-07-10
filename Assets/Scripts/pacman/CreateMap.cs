eb8364890f33b88bbd926872afac1fb9891bea50
John Boyle <boylejm@gmail.com>
2015-07-09 22:14:13 -0700

    refactor code a bit. probably not efficient

--- Assets/Scripts/pacman/CreateMap.cs
+++ Assets/Scripts/pacman/CreateMap.cs
@@ -10,22 +10,28 @@ public class CreateMap : MonoBehaviour {
 
 		int i = 0, j = 0;
 		int numrows = 31, numcols = 28;
-		int[,] result = new int[numrows, numcols];
+		int[,] maparray = new int[numrows, numcols];
+
 		foreach (var row in input.Split('\n')) {
 			j = 0;
 			foreach (var col in row.Trim().Split(',')) {
 				try {
-					int.TryParse(col.Trim(), out result[i, j]);
-					if (result[i,j] == 0) {
-						Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/pacman/pacdot.prefab", typeof(GameObject));
-						GameObject clone = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
-						clone.transform.position = new Vector3(j+1, numrows - i , 0);
-					}
+					int.TryParse(col.Trim(), out maparray[i, j]);
 				}
 				catch { break;}
 				j++;
 			}
 			i++;
+		}
+		//probably not efficient, but loop through maparray
+		for (i = 0; i < numrows; i++){
+			for (j = 0; j < numcols; j++) {
+				if (maparray[i,j] == 0) {
+					Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/pacman/pacdot.prefab", typeof(GameObject));
+					GameObject clone = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
+					clone.transform.position = new Vector3(j + 1, numrows - i , 0);
+				}
+			}
 		}
 	}
 
